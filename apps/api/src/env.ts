9caae56d5f1d29ff213ee0c12f31f89fa7bfd6c1
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2024-09-01 17:28:50 +0530

    config: type safe envs with zod, update tsconfig🔧

---
+++ apps/api/src/env.ts
@@ -0,0 +1,28 @@
+import { ZodError, z } from "zod";
+import { logger } from "@seatsavvy/logger";
+
+const EnvSchema = z.object({
+  NODE_ENV: z.string().default("development"),
+  LOG_LEVEL: z.string().default("debug"),
+  PORT: z.number().default(5500),
+});
+
+export type EnvSchema = z.infer<typeof EnvSchema>;
+
+try {
+  EnvSchema.parse(process.env);
+} catch (error) {
+  if (error instanceof ZodError) {
+    let message = "Missing required values in .env:\n";
+    error.issues.forEach((issue) => {
+      message += issue.path[0] + "\n";
+    });
+    const e = new Error(message);
+    e.stack = "";
+    throw e;
+  } else {
+    logger.error(error);
+  }
+}
+
+export default EnvSchema.parse(process.env);
