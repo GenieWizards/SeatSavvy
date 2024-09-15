f5add0053ae63fafb55b03d4570798e935592493
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2024-09-15 20:26:16 +0530

    feat: add register page🛂

---
+++ apps/web/src/app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+import { RegisterForm } from "@/features/auth/components/register";
+
+export default function Register() {
+  return (
+    <section className="grid place-items-center">
+      <RegisterForm />
+    </section>
+  );
+}
